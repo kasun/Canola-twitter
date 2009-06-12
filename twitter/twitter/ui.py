b5dc62101644a167f86779b2dd09f1c4cf1d44d6
Kasun Herath <kasunh01@gmail.com>
2009-06-12 23:21:11 +0530

    RowRenderer added

--- twitter/twitter/ui.py
+++ twitter/twitter/ui.py
@@ -6,13 +6,15 @@ import locale
 import logging
 
 from terra.core.manager import Manager
+from terra.ui.base import PluginThemeMixin
 
-from model import SendModelFolder,ViewModelFolder
+from model import SendModelFolder,ViewFriendsModelFolder,ViewPublicModelFolder
 
 manager = Manager()
 
 BaseListController = manager.get_class("Controller/Folder")
 EntryDialogModel = manager.get_class("Model/EntryDialog")
+BaseRowRenderer =  manager.get_class("Widget/RowRenderer")
 OptionsControllerMixin = manager.get_class("OptionsControllerMixin")
 
 log = logging.getLogger("plugins.twitter.ui")
@@ -26,7 +28,7 @@ class ListController(BaseListController):
     def cb_on_clicked(self, view, index):
         model = self.model.children[index]
         
-        if type(model) is ViewModelFolder:
+        if type(model) is ViewFriendsModelFolder or type(model) is ViewPublicModelFolder:
             BaseListController.cb_on_clicked(self, view, index)
             return
         
@@ -37,3 +39,29 @@ class ListController(BaseListController):
         dialog = EntryDialogModel("Send Tweets", "Enter text to tweet:",
                                   answer_callback=do_search)
         self.parent.show_notify(dialog)
+        
+class ServiceController(BaseListController, OptionsControllerMixin):
+    terra_type = "Controller/Folder/Task/Apps/Twitter/Service"
+    row_renderer = RowRendererWidget
+    #list_group = "list_video"
+
+    def __init__(self, model, canvas, parent):
+        self.empty_msg = model.empty_msg
+        BaseListController.__init__(self, model, canvas, parent)
+        OptionsControllerMixin.__init__(self)
+        self.model.callback_notify = self._show_notify
+
+    def _show_notify(self, err):
+        """Popup a modal with a notify message."""
+        self.parent.show_notify(err)
+
+class GeneralRowRenderer(PluginThemeMixin, BaseRowRenderer):
+    plugin = "twitter"
+
+    def __init__(self, parent, theme=None):
+        BaseRowRenderer.__init__(self, parent, theme)
+
+class RowRendererWidget(GeneralRowRenderer):
+    row_group="list_item_twitter"
+        
+        
