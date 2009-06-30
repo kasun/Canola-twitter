adb790e83f51174c3a26b7272715ece35edb3470
Kasun Herath <kasunh01@gmail.com>
2009-06-30 23:34:06 +0530

    UTF8 encoding done

--- twitter/twitter/utils.py
+++ twitter/twitter/utils.py
@@ -7,8 +7,12 @@ def getMessageList_from_statuses(statuses):
     messageList = []
     for s in statuses:
         status = {};
-        status["uname"] = s.user.screen_name
-        status["update"] = s.text
-        status["thumb_url"] = s.user.profile_image_url
+        status["uname"] = getUTF8String(s.user.screen_name)
+        status["update"] = getUTF8String(s.text)
+        status["thumb_url"] = getUTF8String(s.user.profile_image_url)
         messageList.append(status)
     return messageList
+
+def getUTF8String(str):
+    #unicodeString = unicode(str,"ascii")
+    return str.encode("utf8")
