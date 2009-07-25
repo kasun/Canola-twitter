2bc481a9d0705bd8297493d55748d7fae2e34ad4
Kasun Herath <kasunh01@gmail.com>
2009-07-25 18:22:08 +0530

    twitpic client integrated into manager.py

--- canola-twitter/twitter/manager.py
+++ canola-twitter/twitter/manager.py
@@ -2,10 +2,23 @@
 
 from terra.core.singleton import Singleton
 from client import Client
+from twitpic import TwitPicClient
 
 class TwitterManager(Singleton,Client):
     
     def __init__(self):
         Singleton.__init__(self)
         Client.__init__(self)
+        self.twitpic_client = TwitPicClient()
+        
+    def uploadAndPostToTwitpic(self, filename, imagedata, message):
+        username = self.getUserName()
+        password = self.getPassWord()
+        
+        fields = [('username', username),('password', password),('message',message)]
+        files = [('media',filename,imagedata)]
+        
+        self.twitpic_client.post_multipart(fields,files)
+        
+    #def uploadToTwitpic(self, filename, imagedata):
 
