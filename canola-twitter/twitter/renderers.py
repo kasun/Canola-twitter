d4cde270fdd6d7fd3a73ad2bbabda2712ff48229
Kasun Herath <kasunh01@gmail.com>
2009-08-08 01:08:48 +0530

    Item renderer for options

---
+++ canola-twitter/twitter/renderers.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import etk
+
+from terra.core.terra_object import TerraObject
+from terra.core.manager import Manager
+
+class TextBoxItemRenderer(etk.KineticRenderer, TerraObject):
+    terra_type = "Renderer/EtkList/LabeledEntryItem"
+
+    def __init__(self, label="", *a, **ka):
+        etk.KineticRenderer.__init__(self, *a, **ka)
+        self.label = label
+        self.args = a
+        self.kargs = ka
+
+    def create_cell(self, canvas):
+        self.entry = etk.Entry(text="")
+        #self.entry = etk.Textblock(text="")
+
+        label = etk.Label(self.label)
+        label.alignment_set(0.0, 0.0)
+
+        vbox = etk.VBox()
+        vbox.border_width_set(0)
+        #vbox.append(label, etk.VBox.START, etk.VBox.FILL, 0)
+        vbox.append(self.entry, etk.VBox.START, etk.VBox.FILL, 0)
+
+        embed = etk.Embed(canvas)
+        embed.add(vbox)
+        embed.show_all()
+
+        return embed.object
+
+    def press(self, cell, row):
+        pass
+
+    def release(self, cell, row):
+        pass
+
+    def update_cell(self, cell, row):
+        pass
+
+    def show_cell(self, cell, x, y, w, h):
+        cell.resize(w - 40, h - 10)
+        cell.move(int(x + 5), int(y + 5))
+        cell.show()
+        
+class MessageItemRenderer(etk.KineticRenderer, TerraObject):
+    terra_type = "Renderer/EtkList/LabeledEntryItem"
+
+    def __init__(self, label="", *a, **ka):
+        etk.KineticRenderer.__init__(self, *a, **ka)
+        self.label = label
+        self.args = a
+        self.kargs = ka
+
+    def create_cell(self, canvas):
+        label = etk.Label(self.label)
+        label.alignment_set(0.0, 0.0)
+
+        vbox = etk.VBox()
+        vbox.border_width_set(0)
+        vbox.append(label, etk.VBox.START, etk.VBox.FILL, 0)
+
+        embed = etk.Embed(canvas)
+        embed.add(vbox)
+        embed.show_all()
+
+        return embed.object
+
+    def press(self, cell, row):
+        pass
+
+    def release(self, cell, row):
+        pass
+
+    def update_cell(self, cell, row):
+        pass
+
+    def show_cell(self, cell, x, y, w, h):
+        cell.resize(w - 40, h - 10)
+        cell.move(int(x + 5), int(y + 5))
+        cell.show()
